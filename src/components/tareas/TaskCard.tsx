58c410e7a30600b407fb4c455a2c23aa442d5bbe
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-06-08 15:13:24 -0400

    add cards

---
+++ src/components/tareas/TaskCard.tsx
@@ -0,0 +1,37 @@
+import { ClipboardList, Plus } from "lucide-react";
+import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+export async function TaskCard() {
+  return (
+    <article className='flex flex-col items-start gap-3 bg-card shadow-md rounded-2xl py-4 px-6 flex-1'>
+      <h3 className='text-sm'>Arquitectura de Información</h3>
+      <Button className='min-w-[220px] text-sm w-full flex items-center justify-between rounded-2xl'>
+        Agregar Topico <Plus size={18} className="dark:text-slate-300" />
+      </Button>
+      <div className='flex items-center justify-between w-full'>
+        <div className='flex -space-x-2'>
+          <Avatar
+            className="inline-block size-8 rounded-full ring-2 ring-white dark:ring-neutral-900"
+          >
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Avatar
+            className="inline-block size-8 rounded-full ring-2 ring-white dark:ring-neutral-900"
+          >
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Avatar
+            className="inline-block size-8 rounded-full ring-2 ring-white dark:ring-neutral-900"
+          >
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <ClipboardList size={18} className="dark:text-slate-300" />
+      </div>
+    </article>
+  )
+}
---
+++ src/components/topicos/TopicCard.tsx
@@ -0,0 +1,26 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { MessagesSquare } from 'lucide-react';
+
+export async function TopicCard() {
+  return (
+    <article className='flex items-start gap-4 w-full px-6 py-4 bg-card shadow-md rounded-lg'>
+      <Avatar
+        className="inline-block size-8 rounded-full ring-2 ring-white dark:ring-neutral-900"
+      >
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className='flex flex-col items-start gap-2'>
+        <strong className="text-xs text-text2 font-semibold">Viviana Uyarte</strong>
+        <div className="flex items-center justify-between gap-4">
+          <p className="flex-1 max-w-[450px] text-balance text-text2/90">Esta información la saque de un sitio web muy interasante donde encuentro datos importantes como realizar...</p>
+          <div className="relative">
+            <MessagesSquare size={18} className="dark:text-slate-300" />
+            <small className="absolute -top-3 -right-1 text-primary font-semibold">3</small>
+          </div>
+        </div>
+        <strong className="self-end text-primary font-semibold pe-6 text-sm">Arquitectuira de Información</strong>
+      </div>
+    </article>
+  )
+}
