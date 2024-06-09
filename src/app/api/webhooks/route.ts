2f1ee13e089d98f0295fd435dc607113dba1749e
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-06-08 22:21:16 -0400

    add logs

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -52,8 +52,8 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  // console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  // console.log('Webhook body:', body)
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  console.log('Webhook body:', body)
 
   if (eventType === 'user.created') {
     console.log('userId created:', evt.data)
